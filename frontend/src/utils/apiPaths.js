9d6f2db386fdde26914c1e78a5c7d60b2308d479
Cripe101 <mhegryanlimpangog@gmail.com>
2026-03-02 12:12:09 +0800

    url update

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:4000";
+export const BASE_URL = "https://aclc-connect-capstone.onrender.com";
 // http://localhost:4000
 // https://aclc-connect-capstone.onrender.com
 
