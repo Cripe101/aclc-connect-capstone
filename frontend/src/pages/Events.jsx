d95bde4e930e173419b0010e392f9e4bd9911b5c
Cripe101 <mhegryanlimpangog@gmail.com>
2026-03-05 14:26:09 +0800

    added searchbar for events

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -5,6 +5,8 @@ import { useEffect, useMemo, useState } from "react";
 import EventsCard from "../components/Cards/EventsCard";
 
 const Events = () => {
+  const [search, setSearch] = useState();
+
   const queryEvents = useQuery({
     queryKey: ["events"],
     queryFn: getPosts,
@@ -17,26 +19,39 @@ const Events = () => {
     // console.log(events);
   }, [queryEvents]);
 
-  const sortedEvents = useMemo(() => {
+  const filteredEvents = useMemo(() => {
     return [...(events || [])]
-      .filter(
-        (item) =>
-          !item.tags?.some(
-            (tag) =>
-              tag.toLowerCase() === "announcement" ||
-              tag.toLowerCase() === "announcements",
-          ),
+      .filter((item) =>
+        item.tags?.some(
+          (tag) =>
+            tag.toLowerCase() === "events" || tag.toLowerCase() === "event",
+        ),
       )
+      .filter((item) => {
+        const query = search.toLowerCase();
+
+        return (
+          item.title?.toLowerCase().includes(query) ||
+          item.tags?.some((tag) => tag.toLowerCase().includes(query))
+        );
+      })
       .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-  }, [events]);
+  }, [events, search]);
 
   return (
     <BlogLayout>
       <div className="p-5">
-        <section className="flex bg-blue-900 p-3 rounded-lg text-white sticky w-full">
+        <section className="flex justify-between bg-blue-900 p-3 rounded-lg text-white sticky w-full">
           <h1 className="text-lg font-bold">Events</h1>
+          <input
+            type="text"
+            placeholder="Search events..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="bg-white w-40 md:w-60 outline-none rounded-lg px-2 py-1 text-black"
+          />
         </section>
-        <EventsCard data={sortedEvents} />
+        <EventsCard data={filteredEvents} />
       </div>
     </BlogLayout>
   );
